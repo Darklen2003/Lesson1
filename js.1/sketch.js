3bcbe1a6a64f45336cdf982970bc61b128d6b8a2
Darklen2003 <ilkapaanian@gmail.com>
2024-09-13 13:09:25 +0300

    second commit

--- js.1/sketch.js
+++ js.1/sketch.js
@@ -35,7 +35,8 @@ function Cloud()
 {
 	noStroke();
 	fill(128,128,128);
-	ellipse(400,50,200,100);
+	ellipse(400,50,120,60);
+	circle(400,50,80);
 	fill(255, 255, 0);
 	circle(100,100,100);
 }
@@ -44,4 +45,6 @@ function Mountain()
 	noStroke();
 	fill(150, 75, 0);
 	triangle(350,500,900,500,600,200);
+	fill(255, 255, 255);
+	triangle(565,240,640,240,600,200);
 }
